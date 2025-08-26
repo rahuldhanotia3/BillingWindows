ded1bf6f5388996351f8dfcd740b953b9ff837f8
rupesh <rupesh.kumar23@manipalcigna.com>
2025-08-26 18:29:05 +0530

    add

--- ProductCRMAPI/Entities/RequestJSON.cs
+++ ProductCRMAPI/Entities/RequestJSON.cs
@@ -73,6 +73,7 @@ namespace ProductCRMAPI.Entities
         public string Status { get; set; }
         public string vchCoverCode { get; set; }
         public string vchCoverPartCode { get; set; }
+        public string vchProductCode { get; set; }
     }
     public class CoverTagging
     {
