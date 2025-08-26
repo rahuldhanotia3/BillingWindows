f4b48ddae4546c2247454c564c9b51fd13fb07ed
rupesh <rupesh.kumar23@manipalcigna.com>
2025-08-26 16:30:13 +0530

    add

--- ProductCRMAPI/Form1.cs
+++ ProductCRMAPI/Form1.cs
@@ -170,6 +170,7 @@ namespace ProductCRMAPI
                                 objCoverPartDetail.Status = SubCover.Rows[t3]["Status"].ToString();
                                 objCoverPartDetail.vchCoverCode = SubCover.Rows[t3]["vchCoverCode"].ToString();
                                 objCoverPartDetail.vchCoverPartCode = SubCover.Rows[t3]["vchCoverPartCode"].ToString();
+                                objCoverPartDetail.vchProductCode= SubCover.Rows[t3]["vchProductCode"].ToString();  
                                 objListCoverPartDetails.Add(objCoverPartDetail);
                             }
 
